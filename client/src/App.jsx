47a7291ae071a2f85cb2f43e8762f6f89dedea66
BRID19 <74292639+BRID19@users.noreply.github.com>
2026-01-01 19:09:15 +0100

    Day One page created.

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DayOne from './pages/Day1.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl text-amber-200 font-bold underline">Hello, World</h1>
+      <DayOne />
+    </div>
+  )
+}
+
+export default App
