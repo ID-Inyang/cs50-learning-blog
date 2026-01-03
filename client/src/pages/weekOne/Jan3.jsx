0ef9dd1658b40c3532f98076ef1ae362cc1c47d8
BRID19 <74292639+BRID19@users.noreply.github.com>
2026-01-03 06:06:44 +0100

    made a change

--- client/src/pages/weekOne/Jan3.jsx
+++ client/src/pages/weekOne/Jan3.jsx
@@ -1,7 +1,14 @@
 import React from "react"
 
 function Jan3() {
-    return ()
+    return (
+        <main className="p-8 min-h-screen">
+            <div>
+                <h1>Day 2: </h1>
+                <h6>January 3rd 2026</h6>
+            </div>
+        </main>
+    )
 }
 
 export default Jan3
